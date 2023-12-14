c7fa51bda3fc3e6d808a5542b2b9cd470c9584a9
David Haberthür <email@davidhaberthuer.ch>
2023-12-14 15:22:00 +0100

    Also archive overview photos

--- rsync-sticklebacks.sh
+++ rsync-sticklebacks.sh
@@ -1,5 +1,5 @@
 echo "2214 --> archive"
-rsync --verbose --recursive --compress --times --update --omit-dir-times --prune-empty-dirs --include="*/" --include="*.?og" --include="*.c?v" --include="*.?oi" --include="*.?at" --include="*_spr*.bmp" --include="*.txt" --include="*.md" --include="*.mp" --include="*.sb" --include="*.tf" --include="*.info" --include="*.?nc" --include="*.bkp" --include="*.ctt" --include="*.?if" --exclude="*_rectmp.log" --exclude="*"  ~/2214/IEE\ Stickleback/ ~/research-storage-uct/Archiv_Tape/IEE\ Stickleback/
+rsync --verbose --recursive --compress --times --update --omit-dir-times --prune-empty-dirs --include="*/" --include="*.?og" --include="*.c?v" --include="*.?oi" --include="*.?at" --include="*_spr*.bmp" --include="*.txt" --include="*.md" --include="*.mp" --include="*.sb" --include="*.tf" --include="*.info" --include="*.?nc" --include="*.bkp" --include="*.ctt" --include="*.jpg" --include="*.?if" --exclude="*_rectmp.log" --exclude="*"  ~/2214/IEE\ Stickleback/ ~/research-storage-uct/Archiv_Tape/IEE\ Stickleback/
 echo "2214 --> research storage Ben"
 rsync --verbose --recursive --compress --times --update --omit-dir-times --prune-empty-dirs --include="*/" --exclude="*_rectmp.log" ~/2214/IEE\ Stickleback/ ~/research-storage-ben/BenSulser/microCT\ Stickleback
 echo "2214 (everything but projections) -> anaklin25 FastSSD"
