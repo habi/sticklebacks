5c32023c3fd05e01cf369b14bcfc13be397db387
David Haberthür <email@davidhaberthuer.ch>
2024-03-01 11:33:02 +0100

    rsync script less verbose while doing the same

--- rsync-sticklebacks.sh
+++ rsync-sticklebacks.sh
@@ -1,10 +1,12 @@
 echo "2214 --> archive"
-rsync --verbose --recursive --compress --times --update --omit-dir-times --prune-empty-dirs --include="*/" --include="*.?og" --include="*.c?v" --include="*.?oi" --include="*.?at" --include="*_spr*.bmp" --include="*.txt" --include="*.md" --include="*.mp" --include="*.sb" --include="*.tf" --include="*.info" --include="*.?nc" --include="*.bkp" --include="*.ctt" --include="*.jpg" --include="*.?if" --exclude="*_rectmp.log" --exclude="*"  ~/2214/IEE\ Stickleback/ ~/research-storage-uct/Archiv_Tape/IEE\ Stickleback/
+rsync --verbose --recursive --times --update --omit-dir-times --prune-empty-dirs --include="*/" --exclude="*.png" --exclude="*_rectmp.log"  ~/2214/IEE\ Stickleback/ ~/research-storage-uct/Archiv_Tape/IEE\ Stickleback/
 echo "2214 --> research storage Ben"
-rsync --verbose --recursive --compress --times --update --omit-dir-times --prune-empty-dirs --include="*/" --exclude="*_rectmp.log" ~/2214/IEE\ Stickleback/ ~/research-storage-ben/BenSulser/microCT\ Stickleback
+rsync --verbose --recursive --times --update --omit-dir-times --prune-empty-dirs --include="*/" --exclude="*_rectmp.log" ~/2214/IEE\ Stickleback/ ~/research-storage-ben/BenSulser/microCT\ Stickleback
 echo "2214 (everything but projections) -> anaklin25 FastSSD"
-rsync --verbose --recursive --compress --times --update --omit-dir-times --prune-empty-dirs --include="*/" --exclude="*.?if" ~/2214/IEE\ Stickleback/ /media/habi/Fast_SSD/IEE\ Stickleback/
-echo "anaklin25 FastSSD (all PNGs we made)--> research storage Ben"
-rsync --verbose --recursive --compress --times --update --omit-dir-times --prune-empty-dirs --include="*/" --include="*.png" --exclude="*" /media/habi/Fast_SSD/IEE\ Stickleback/ ~/research-storage-ben/BenSulser/microCT\ Stickleback
-echo "2214 (logfiles) --> repository subfolder"
-rsync --verbose --recursive --compress --times --update --omit-dir-times --prune-empty-dirs --include="*/" --include="*.?og" --include="*.c?v" --include="*.?oi" --include="*.?at" --include="*.txt" --include="*.md" --include="*.mp" --include="*.sb" --include="*.tf" --include="*.info" --include="*.?nc" --include="*.bkp" --include="*.ctt" --exclude="*_rectmp.log" --exclude="*"  ~/2214/IEE\ Stickleback/ ~/P/Documents/IEE/Sulser\ Sticklebacks/logfiles/
+rsync --verbose --recursive --times --update --omit-dir-times --prune-empty-dirs --include="*/" --exclude="*.?if" --exclude="*_rectmp.log" ~/2214/IEE\ Stickleback/ /media/habi/Fast_SSD/IEE\ Stickleback/
+echo "anaklin25 FastSSD (all PNGs and .logs we made)--> research storage Ben"
+rsync --verbose --recursive --times --update --omit-dir-times --prune-empty-dirs --include="*/" --include="*.log" --include="*.png" --exclude="*_rectmp.log" --exclude="*" /media/habi/Fast_SSD/IEE\ Stickleback/ ~/research-storage-ben/BenSulser/microCT\ Stickleback
+echo "anaklin25 FastSSD (all text files)--> archive"
+rsync --verbose --recursive --times --update --omit-dir-times --prune-empty-dirs --include="*/" --include="*.log" --include="*.md" --include="*.txt" --exclude="*_rectmp.log" --exclude="*" /media/habi/Fast_SSD/IEE\ Stickleback/ ~/research-storage-ben/BenSulser/microCT\ Stickleback
+echo "Archive (logfiles) --> repository subfolder"
+rsync --verbose --recursive --times --update --omit-dir-times --prune-empty-dirs --include="*/" --include="*.?og" --include="*.?nc" --exclude="*_rectmp.log" --exclude="*" ~/research-storage-uct/Archiv_Tape/IEE\ Stickleback/ ~/P/Documents/IEE/Sulser\ Sticklebacks/logfiles/
