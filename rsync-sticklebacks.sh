eb88dc74bb14ec1bc88520946f86a152b1ceb318
David Haberthür <email@davidhaberthuer.ch>
2026-07-15 15:51:29 +0200

    No intermediate step on my machine

--- rsync-sticklebacks.sh
+++ rsync-sticklebacks.sh
@@ -7,13 +7,13 @@ echo "2214 --> archive"
 rsync $options --exclude="*.png" ~/2214/IEE\ Stickleback/ ~/research_storage_uct/Archiv_Tape/IEE\ Stickleback/
 echo "2214 --> research storage Ben"
 rsync $options ~/2214/IEE\ Stickleback/ ~/research_storage_ben/microCT_Stickleback/
-echo "2214 (everything but projections) -> anamyct05 FastSSD"
-rsync $options --exclude="*.?if" ~/2214/IEE\ Stickleback/ /media/habi/Fast_SSD/IEE\ Stickleback/
-echo "anamyct05 FastSSD (all PNGs and .logs we made)--> research storage Ben"
-rsync $options /media/habi/Fast_SSD/IEE\ Stickleback/ ~/research_storage_ben/microCT_Stickleback/
-echo "anamyct05 FastSSD (all text files)--> archive"
-rsync $options --include="*.log" --include="*.md" --include="*.txt" --exclude="*" /media/habi/Fast_SSD/IEE\ Stickleback/ ~/research_storage_ben/microCT_Stickleback/
-echo "All log files (and label-checking files) from research_storage Ben --> archive"
-rsync $options --include="*.?og" --include="*.?nc" --include="*.md" --include="*.Labels.Check.png" --exclude="._*" --exclude="*" ~/research_storage_ben/microCT_Stickleback/ ~/research_storage_uct/Archiv_Tape/IEE\ Stickleback/
+# echo "2214 (everything but projections) -> anamyct05 FastSSD"
+# rsync $options --exclude="*.?if" ~/2214/IEE\ Stickleback/ /media/habi/Fast_SSD/IEE\ Stickleback/
+# echo "anamyct05 FastSSD (all PNGs and .logs we made) --> research storage Ben"
+# rsync $options /media/habi/Fast_SSD/IEE\ Stickleback/ ~/research_storage_ben/microCT_Stickleback/
+# echo "research storage Ben (all text files) --> archive"
+# rsync $options --include="*.log" --include="*.md" --include="*.txt" --exclude="*" /media/habi/Fast_SSD/IEE\ Stickleback/ ~/research_storage_ben/microCT_Stickleback/
+echo "All log, text and label-checking files from research_storage Ben --> archive"
+rsync $options --include="*.?og" --include="*.?nc" --include="*.md" --include="*.txt" --include="*.Labels.Check.png" --exclude="._*" --exclude="*" ~/research_storage_ben/microCT_Stickleback/ ~/research_storage_uct/Archiv_Tape/IEE\ Stickleback/
 echo "Archive (logfiles, photos, markdown and label-checking files) --> repository subfolder"
 rsync $options --include="*.?og" --include="*.?nc" --include="*.jpg" --include="*.md" --include="*.Labels.Check.png" --exclude="*" ~/research_storage_uct/Archiv_Tape/IEE\ Stickleback/ ~/P/Documents/IEE/Sulser\ Sticklebacks/logfiles/
