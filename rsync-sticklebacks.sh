6c7bb262e8ec38f07f44ad3c6220a0b3a8794813
David Haberthür <email@davidhaberthuer.ch>
2026-08-11 17:55:03 +0200

    Exclude some more stuff

--- rsync-sticklebacks.sh
+++ rsync-sticklebacks.sh
@@ -14,7 +14,9 @@ options=(
     --info=progress2        # Replaces line-by-line --verbose with a single updating progress line
     --exclude='*SubScan?/*'
     --exclude='*_rectmp.log'
+    --exclude='*.tmp'
     --exclude='Thumbs.db'
+    --exclude='*DS_Store'
     --include='*/'
 )
 
@@ -29,7 +31,7 @@ echo "2214 (everything but projections) -> anamyct05 FastSSD"
 rsync "${options[@]}" --exclude="*.?if" ~/2214/IEE\ Stickleback/ /media/habi/Fast_SSD/IEE\ Stickleback/
 
 # echo "research_storage Ben --> anamyct05 FastSSD (all PNGs and things Ben and Sheila)"
-# rsync "${options[@]}" --exclude="*.?if" --exclude="*.nrrd" --exclude="*.ply" --exclude="*.ckpt" --exclude="*.mrml" --exclude="*.json" --exclude="*.vp" --exclude="*.tar" --exclude="*.h5" --exclude="*.ctbl" ~/research_storage_ben/microCT_Stickleback/ /media/habi/Fast_SSD/IEE\ Stickleback/
+# rsync "${options[@]}" --exclude="*.?if" --exclude="*.nrrd" --exclude="*.ply" --exclude="*.R" --exclude="*.ckpt" --exclude="*.zip" --exclude="*.doc*" --exclude="*.mr*"--exclude="*.json" --exclude="*.py" --exclude="*.ijm" --exclude="*.vp" --exclude="*.vtk" --exclude="*.tar" --exclude="*.nts" --exclude="*.h5" --exclude="*Rhistory" --exclude="*.pdf" --exclude="*.ctbl" ~/research_storage_ben/microCT_Stickleback/ /media/habi/Fast_SSD/IEE\ Stickleback/
 
 echo "anamyct05 FastSSD (all PNGs and .logs we made, but no .zarr folders) --> research storage Ben"
 rsync "${options[@]}" --exclude="*.zarr/***" /media/habi/Fast_SSD/IEE\ Stickleback/ ~/research_storage_ben/microCT_Stickleback/
