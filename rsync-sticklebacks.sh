894c30620c8412d60ff61b2665ad53da7bd203db
David Haberthür <email@davidhaberthuer.ch>
2026-08-14 17:46:41 +0200

    Different order of calling options

--- rsync-sticklebacks.sh
+++ rsync-sticklebacks.sh
@@ -3,23 +3,17 @@ echo -en "\033]0;Stickleback synchronization\a"
 
 # Set options
 options=(
+#    --info=progress2        # Replaces line-by-line --verbose with a single updating progress line
+    --verbose
     --archive
     --update
-    --whole-file            # Bypasses CPU delta calculations; streams files directly (ideal for fast SSDs/LAN)
-    --inplace               # Writes directly to target files without creating temporary split files first
-    --omit-dir-times
     --prune-empty-dirs
-    --no-owner              # Avoids chown overhead over network/external mounts
-    --no-group              # Avoids chgrp overhead over network/external mounts
-    --verbose
-#    --info=progress2        # Replaces line-by-line --verbose with a single updating progress line
     --exclude='*SubScan?/*'
     --exclude='*_rectmp.log'
-    --exclude='*.zip'
+    --exclude='*.zip'    
     --exclude='*.tmp'
     --exclude='Thumbs.db'
     --exclude='*DS_Store'
-    --include='*/'
 )
 
 # Sync stuff
@@ -33,13 +27,13 @@ echo "2214 (everything but projections) -> anamyct05 FastSSD"
 rsync "${options[@]}" --exclude="*.?if" ~/2214/IEE\ Stickleback/ /media/habi/Fast_SSD/IEE\ Stickleback/
 
 # echo "research_storage Ben --> anamyct05 FastSSD (all PNGs and things Ben and Sheila)"
-# rsync "${options[@]}" --exclude="*.?if" --exclude="*.nrrd" --exclude="*.ply" --exclude="*.R" --exclude="*.ckpt" --exclude="*.zip" --exclude="*.doc*" --exclude="*.mr*"--exclude="*.json" --exclude="*.py" --exclude="*.ijm" --exclude="*.vp" --exclude="*.vtk" --exclude="*.tar" --exclude="*.nts" --exclude="*.h5" --exclude="*Rhistory" --exclude="*.pdf" --exclude="*.ctbl" ~/research_storage_ben/microCT_Stickleback/ /media/habi/Fast_SSD/IEE\ Stickleback/
+# rsync "${options[@]}" --exclude="*.?if" --exclude="*.nrrd" --exclude="*.ply" --exclude="*.R" --exclude="*.ckpt" --exclude="*.zip" --exclude="*.doc*" --exclude="*.mr*" --exclude="*.json" --exclude="*.py" --exclude="*.ijm" --exclude="*.vp" --exclude="*.vtk" --exclude="*.tar" --exclude="*.nts" --exclude="*.h5" --exclude="*Rhistory" --exclude="*.pdf" --exclude="*.ctbl" ~/research_storage_ben/microCT_Stickleback/ /media/habi/Fast_SSD/IEE\ Stickleback/
 
 echo "anamyct05 FastSSD (all PNGs and .logs we made, but no .zarr folders) --> research storage Ben"
-rsync "${options[@]}" --exclude="*.zarr/***" /media/habi/Fast_SSD/IEE\ Stickleback/ ~/research_storage_ben/microCT_Stickleback/
+rsync "${options[@]}" --exclude="*.zarr" /media/habi/Fast_SSD/IEE\ Stickleback/ ~/research_storage_ben/microCT_Stickleback/
 
 echo "All log, text and label-checking files from research_storage Ben --> archive"
-rsync "${options[@]}" --include="*.?og" --include="*.?nc" --include="*.md" --include="*.txt" --include="*.Labels.Check.png" --include="2024_Fish_Data_Lynn.csv" --exclude="._*" --exclude="*" ~/research_storage_ben/microCT_Stickleback/ ~/research_storage_uct/Archiv_Tape/IEE\ Stickleback/
+rsync "${options[@]}" --include="*.?og" --include="*.?nc" --include="*.md" --include="*.txt" --include="*.Labels.Check.png" --include="2024_Fish_Data_Lynn.csv" --include="*/" --exclude="._*" --exclude="*" ~/research_storage_ben/microCT_Stickleback/ ~/research_storage_uct/Archiv_Tape/IEE\ Stickleback/
 
 echo "Archive (logfiles, photos, markdown and label-checking files) --> repository subfolder"
-rsync "${options[@]}" --include="*.?og" --include="*.?nc" --include="*.jpg" --include="*.md" --include="*.Labels.Check.png" --exclude="*" ~/research_storage_uct/Archiv_Tape/IEE\ Stickleback/ ~/P/Documents/IEE/Sulser\ Sticklebacks/logfiles/
+rsync "${options[@]}" --include="*.?og" --include="*.?nc" --include="*.jpg" --include="*.md" --include="*.Labels.Check.png" --include="*/" --exclude="*" ~/research_storage_uct/Archiv_Tape/IEE\ Stickleback/ ~/P/Documents/IEE/Sulser\ Sticklebacks/logfiles/
